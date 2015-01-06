ef020b74735c7c337b96190f1e537f78ec25e7e5
staypufd <harvie.griffith@austincc.edu>
2015-01-06 12:26:55 -0600

    Initial commit

---
+++ src/edu/austincc/main/App.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package edu.austincc.main;
+
+/**
+ * @author javauser
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("hello");
+
+	}
+
+}
